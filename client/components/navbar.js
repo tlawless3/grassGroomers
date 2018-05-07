f0f6916731699a1017a1df9f943d819a0d7dcd57
Thomas Lawless <tlawless94@gmail.com>
2018-05-07 11:49:34 -0500

    install semantic-ui navbar toggle between mobile and desktop

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,31 +3,22 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Responsive} from 'semantic-ui-react'
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
 
+const Navbar = ({ handleClick, isLoggedIn }) => {
+  
+  return(
+    <div>
+      <Responsive minWidth={750} >
+        <p>Desktop</p>
+      </Responsive>
+      <Responsive maxWidth={750} >
+        <p>Mobile</p>
+      </Responsive>
+    </div>
+  )
+}
 /**
  * CONTAINER
  */
