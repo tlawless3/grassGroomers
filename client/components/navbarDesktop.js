613f8ccd7530186812c8a61e5b406c541b62695f
Thomas Lawless <tlawless94@gmail.com>
2018-05-10 16:10:01 -0500

    responsive navbar implemented

--- client/components/navbarDesktop.js
+++ client/components/navbarDesktop.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import {Menu} from 'semantic-ui-react'
 
-const NavbarDesktop = () => {
+const NavbarDesktop = ({
+  activeItem,
+  isLoggedIn,
+  handleItemClick
+}) => {
   return (
     <div>
-      <p>Desktop</p>
+      {isLoggedIn ? (
+      <Menu size='small'>
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={handleItemClick} />
+        <Menu.Item name='appointments' active={activeItem === 'appointments'} onClick={handleItemClick} />
+        <Menu.Menu position='right'>
+          <Menu.Item name='Log Out' />
+        </Menu.Menu>
+      </Menu>
+      ) :
+      (
+        <Menu>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={handleItemClick} />
+          <Menu.Menu position='right'>
+            <Menu.Item name='login' active={activeItem === 'login'} onClick={handleItemClick} />
+            <Menu.Item name='signUp' active={activeItem === 'signUp'} onClick={handleItemClick} />
+          </Menu.Menu>
+        </Menu>
+      )}
     </div>
   )
 }
--- client/components/navbarMobile.js
+++ client/components/navbarMobile.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Menu, Dropdown} from 'semantic-ui-react'
 
-const NavbarMobile = () => {
+const NavbarMobile = ({
+  isLoggedIn,
+  handleItemClick
+}) => {
+
   return (
     <div>
-      <p>Mobile</p>
+      {isLoggedIn ? (
+        <Menu>
+            <Dropdown item text='Menu'>
+              <Dropdown.Menu>
+                <Dropdown.Item name='home' onClick={handleItemClick}>Home</Dropdown.Item>
+                <Dropdown.Item name='schedule' onClick={handleItemClick}>Appointments</Dropdown.Item>
+                <Dropdown.Item name='home' onClick={handleItemClick}>Log Out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+        </Menu>
+      ) :
+      (
+        <Menu>
+            <Dropdown item text='Menu'>
+              <Dropdown.Menu>
+                <Dropdown.Item name='home' onClick={handleItemClick}>Home</Dropdown.Item>
+                <Dropdown.Item name='login' onClick={handleItemClick}>Log In</Dropdown.Item>
+                <Dropdown.Item name='signUp' onClick={handleItemClick}>Sign Up</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+        </Menu>
+      )}
     </div>
   )
 }
