3229f5dfbf1c498d994218cbaff1ad48fa24d8bb
Thomas Lawless <tlawless94@gmail.com>
2018-06-02 13:22:05 -0500

    calendar takes up size of screen

--- client/components/appointmentCalendar.js
+++ client/components/appointmentCalendar.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Button} from 'semantic-ui-react'
 import InfiniteCalendar from 'react-infinite-calendar';
-import 'react-infinite-calendar/styles.css';
 
-const AppointmentCalendar = () => {
-  var today = new Date();
-  var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
-  return (
-    <div>
-      <InfiniteCalendar
-      width={400}
-      height={600}
-      selected={today}
-      disabledDays={[0,6]}
-      minDate={lastWeek}
-      />
-    </div>
-  )
+class AppointmentCalendar extends Component{
+  constructor(props){
+    super(props)
+
+    this.state={
+      startingDate: null,
+      screenHeight: 0,
+      screenWidth: 0
+    }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+  }
+
+  componentDidMount(){
+    this.updateWindowDimensions();
+    this.setCurrentDate();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  setCurrentDate() {
+    let today = new Date()
+    this.setState({
+      startingDate:  new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2)
+    })
+  }
+
+  updateWindowDimensions() {
+    this.setState({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight - 187
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <InfiniteCalendar
+        width={this.state.screenWidth}
+        height={this.state.screenHeight}
+        selected={this.state.today}
+        disabledDays={[0,6]}
+        minDate={this.state.lastWeek}
+        />
+      </div>
+    )
+  }
 }
 
 export default AppointmentCalendar
